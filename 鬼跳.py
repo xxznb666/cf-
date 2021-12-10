198c81e6afc5bd48c61e4d86ad85aab771978e95
xxznb666 <76636332+xxznb666@users.noreply.github.com>
2021-12-11 00:52:29 +0800

    Update 鬼跳.py
    先按方向键，然后再按触发键，这样就可以变成网络身法帝了

--- 鬼跳.py
+++ 鬼跳.py
@@ -9,16 +9,16 @@ def 鬼跳():
 			flag=1
 			if flag == 1:
 				print(win32api.GetCursorPos())
-				print('我跳!')
-				key_down(0x39)
+				print('我跳尼玛!')
+				key_down(0x39)#按住空格键
 				time.sleep(0.01)
-				key_up(0x39)
+				key_up(0x39)#松开空格键
 				time.sleep(0.01)
-				key_down(0x2a)
+				key_down(0x2a)#按住蹲键(shift,我游戏里是调成shift的，所以略略略)
 				if win32api.GetAsyncKeyState(0x04)&0x8000 != 32768:
 					flag=0
 					print(flag)
-					key_up(0x2a)
+					key_up(0x2a)#松开盾键
 
 		
 		else:
