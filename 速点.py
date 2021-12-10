528fae29de330dc3f3975bc8f60d4dc99fe3efc3
xxznb666 <76636332+xxznb666@users.noreply.github.com>
2021-12-11 00:55:41 +0800

    Update 速点.py

--- 速点.py
+++ 速点.py
@@ -8,11 +8,9 @@ def sudian():
 			print(1)
 			flag=1
 			while flag == 1:
-				#controller.click(Button.left,1)
 				win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
 				win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 				print(win32api.GetCursorPos())
-				#win32api.SetCursorPos((878, 431))
 				print('速点一次')
 				time.sleep(0.065)
 				if win32api.GetAsyncKeyState(0x02)&0x8000 != 32768:
